165e654c28ffc51dc6bb3ee21dab3ba7b31c7a34
lamlao1532 <124331728+lamlao1532@users.noreply.github.com>
2023-07-04 16:32:22 +0700

    Create Bai_263.py

---
+++ Bai_263.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        if n < 2: 
+            return True
+        while n != 1:
+            if n % 2 == 0:
+                n /=2
+            elif n % 3 == 0:
+                n /= 3
+            elif n % 5 == 0:
+                n /=5
+            else: 
+                return False
+        return True
